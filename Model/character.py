2f9ad6b3c17edcce43be3c1321beaf1f4e44893a
Leonardo <leonardo.appio@gmail.com>
2022-01-09 18:53:56 -0300

    added abstractmethod at character methods

--- Model/character.py
+++ Model/character.py
@@ -1,4 +1,5 @@
-class Character():
+from abc import ABC, abstractmethod
+class Character(ABC):
     def __init__(self,name,attack,defense,life):
         self.__name = name
         self.__attack = attack
@@ -6,34 +7,42 @@ class Character():
         self.__life = life
     
     @property
+    @abstractmethod
     def name(self):
         return self.__name
     
     @name.setter
+    @abstractmethod
     def name(self,name):
         self.__name = name
     
     @property
+    @abstractmethod
     def attack(self):
         return self.__attack
     
     @attack.setter
+    @abstractmethod
     def attack(self,attack):
         self.__attack = attack
     
     @property
+    @abstractmethod
     def defense(self):
         return self.__defense
     
     @defense.setter
+    @abstractmethod
     def defense(self,defense):
         self.__defense = defense
 
     @property
+    @abstractmethod
     def life(self):
         return self.__life
     
     @life.setter
+    @abstractmethod
     def life(self,life):
         self.__life = life
     
