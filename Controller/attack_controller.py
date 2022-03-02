f1223870d48d45a9acf6198201154699c06e2898
Leonardo <leonardo.appio@gmail.com>
2022-03-02 12:19:28 -0300

    added list of attacks at AttackController

--- Controller/attack_controller.py
+++ Controller/attack_controller.py
@@ -3,12 +3,17 @@ import random
 
 class AttackController():
     def __init__(self):
-        ...
+        self.__attacks = []
     
     def generate_new_random_attack(self,battle_number):
         inferior_limit = (battle_number * 10) + 5
         upper_limit = (battle_number * 12) + 10
         random_attack = random.randint(inferior_limit, upper_limit)
 
-        return Attack('Boss Attack', random_attack)
-        
+        attack = Attack('Boss Attack', random_attack)
+        self.__attacks.append(attack)
+        return attack
+    
+    @property
+    def attacks(self):
+        return self.__attacks
