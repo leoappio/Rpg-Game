623f2f09cfa0f8c3f351b7e8b18400b765159d84
Leonardo <leonardo.appio@gmail.com>
2022-03-02 12:22:44 -0300

    added list of defenses at DefenseController

--- Controller/defense_controller.py
+++ Controller/defense_controller.py
@@ -3,11 +3,20 @@ import random
 
 class DefenseController():
     def __init__(self):
-        ...
+        self.__defenses = []
     
     def generate_new_random_defense(self,battle_number):
         inferior_limit = (battle_number * 10) + 5
         upper_limit = (battle_number * 12) + 10
         random_defense = random.randint(inferior_limit, upper_limit)
 
-        return Defense('Boss Defense', random_defense)
+        defense = Defense('Boss Defense', random_defense)
+        self.__defenses.append(defense)
+
+        return defense
+    
+
+    @property
+    def defenses(self):
+        return self.__defenses
+
