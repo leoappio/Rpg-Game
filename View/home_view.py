31f1d7883dfd44f4c17aedd8dc9406850bb44f1d
Leonardo <leonardo.appio@gmail.com>
2022-03-04 14:27:34 -0300

    added home screen using PySimpleGUI

--- View/home_view.py
+++ View/home_view.py
@@ -1,31 +1,63 @@
 from View.base_view import BaseView
 from Model.Exceptions.InvalidChoiceException import InvalidChoiceException
+import PySimpleGUI as sg
+
 
 class HomeView(BaseView):
-    
-    def show_home_screen_options(self, coins_quantity, current_battle):
-        self.clear_screen()
-        print('-----------Home Screen----------')
-        print('|',coins_quantity,'Coins | Current Battle:',current_battle,'|')
-        print('1- New Battle')
-        print('2- Fighters Menu')
-        print('3- History')
 
-        while True:
-            try:
-                option = input("Enter your choice:")
-                if option in ['1','2','3']:
-                    return int(option)
-                else:
-                    raise InvalidChoiceException()
-            except InvalidChoiceException as e:
-                print(e)
+    def __init__(self):
+        self.__window = None
+        self.init_components(50,1)
+    
+
+    def show_home_screen_options(self, coins_quantity, current_battle):
+        self.init_components(coins_quantity,current_battle)
+        button, values = self.__window.Read()
+        
+        option = 0
+
+        if values['1']:
+            option = 1
+        elif values['2']:
+            option = 2
+        elif values['3']:
+            option = 3
+
+        if values ['0'] or button in (None,'Cancel'):
+            option = 0
+
+        self.close()
+        return option
+
+
+    def close(self):
+        self.__window.Close()
+
+
+    def init_components(self, coins_quantity, current_battle):
+        text_player_data = str(coins_quantity) +' Coins || Current Battle: '+str(current_battle)
+        sg.ChangeLookAndFeel('DarkTeal4')
+        layout = [
+            [sg.Text('Welcome to RPG Game!', font =("Helvetica",25))],
+            [sg.Text(text_player_data, font =("Helvetica",15))],
+            [sg.Text('Choose your option', font =("Helvetica",15))],
+            [sg.Radio('New Battle',"RD1" ,key = '1')],
+            [sg.Radio('Fighters Menu',"RD1" ,key = '2')],
+            [sg.Radio('History',"RD1" ,key = '3')],
+            [sg.Radio('Exit',"RD1" ,key = '0')],
+            [sg.Button('Confirm',sg.Cancel('Cancel'))]
+        ]
+
+        self.__window = sg.Window('RPG Game - POO 2').Layout(layout)
+
 
     def defeat(self):
         print('You Lost!')
 
+
     def victory(self):
         input('You Won! Press enter to continue')
+
 
     def show_history(self, data):
         self.clear_screen()
@@ -33,10 +65,12 @@ class HomeView(BaseView):
             print(text)
         input('Press enter to continue')
 
+
     def ending(self):
         self.clear_screen()
         input('Congratulations, you finished the game!')
         input('Press enter to continue')
+
 
     def select_fighters_for_battle(self):
         self.clear_screen()
