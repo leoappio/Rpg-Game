71524d830d2450cb505a525b14386b3807b19c5a
Leonardo <leonardo.appio@gmail.com>
2022-03-09 11:34:30 -0300

    changed boss controller to use DAO class

--- Controller/boss_controller.py
+++ Controller/boss_controller.py
@@ -2,13 +2,14 @@ from View.boss_view import BossView
 from Model.boss import Boss
 from Controller.attack_controller import AttackController
 from Controller.defense_controller import DefenseController
+from DAO.bosses_dao import BossesDAO
 
 class BossController():
     def __init__(self):
         self.__boss_view = BossView()
         self.__attack_controller = AttackController()
         self.__defense_controller = DefenseController()
-        self.__bosses = []
+        self.__bosses_DAO = BossesDAO()
     
     def generate_new_boss(self, battle_number):
         
@@ -18,10 +19,13 @@ class BossController():
         life = battle_number * 50
 
         generated_boss = Boss('Boss '+str(battle_number), boss_attack, boss_defense, life)
-        self.__bosses.append(generated_boss)
+
+        boss_id = len(self.__bosses_DAO.get_all()) + 1
+        self.__bosses_DAO.add(boss_id,generated_boss)
+
         return generated_boss
     
 
     @property
     def bosses(self):
-        return self.__bosses
+        return self.__bosses_DAO.get_all()
